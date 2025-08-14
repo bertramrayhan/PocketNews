a892d5cfe54089614ef5d0865751ba0529707c70
bertramrayhan <bertramrayhan1357@gmail.com>
2025-08-14 12:38:35 +0700

    menghubungkan bottom navigation dengan fragment container

--- app/src/main/java/com/example/pocketnews/MainActivity.java
+++ app/src/main/java/com/example/pocketnews/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.pocketnews;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navbar);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) bottomNavigationView.getLayoutParams();
+            params.bottomMargin = systemBars.bottom;
+            bottomNavigationView.setLayoutParams(params);
+
+            return WindowInsetsCompat.CONSUMED;
         });
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
